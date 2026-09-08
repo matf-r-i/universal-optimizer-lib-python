2b95dcc13d76a04421e31a300bb3449e2799737f
nenad.pesic <nenad.pesic@kupujemprodajem.com>
2026-09-08 21:11:15 +0200

    Initial commit

---
+++ uo/algorithm/metaheuristic/simulated_annealing/sa_neighborhood_permutation.py
@@ -0,0 +1,7 @@
+
+from uo.algorithm.metaheuristic.simulated_annealing.sa_neighborhood import SaNeighborhood
+
+class SaNeighborhoodPermutation(SaNeighborhood):
+    def __init__(self, dimension: int, k: int = 1) -> None: 
+        
+        pass

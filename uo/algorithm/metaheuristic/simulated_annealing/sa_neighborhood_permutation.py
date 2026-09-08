455a648b3a64bc9ac7e82cb5a45b8ccffca6b208
nenad.pesic <nenad.pesic@kupujemprodajem.com>
2026-09-08 21:15:52 +0200

    add class initializer with type checking

--- uo/algorithm/metaheuristic/simulated_annealing/sa_neighborhood_permutation.py
+++ uo/algorithm/metaheuristic/simulated_annealing/sa_neighborhood_permutation.py
@@ -2,6 +2,24 @@
 from uo.algorithm.metaheuristic.simulated_annealing.sa_neighborhood import SaNeighborhood
 
 class SaNeighborhoodPermutation(SaNeighborhood):
-    def __init__(self, dimension: int, k: int = 1) -> None: 
-        
-        pass
+    def __init__(self, dimension: int, k: int = 1) -> None:
+        """
+        Create new `SaNeighboorhoodPermutation` instance
+
+        :param dimension: length of the permutation used as representation 
+        :type dimension: int
+        :param k: number of swaps that form a single move, defaults to 1
+        :type k: int, optional
+        """
+
+        if not isinstance(dimension, int):
+            raise TypeError("Parameter 'dimension' must be 'int'.")
+        if dimension < 2:
+            raise ValueError("Parameter 'dimension' must be at least 2.")
+        if not isinstance(k, int):
+            raise TypeError("Parameter 'k' must be 'int'.")
+        if k < 1:
+            raise ValueError("Parameter 'k' must be greater than zero.")
+        self.dimension: int = dimension
+        self.k: int = k
+    
